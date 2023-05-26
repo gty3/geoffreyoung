c02b987aa8ca1ea93022a8dbd27d65f061f493d0
gty <gefyoung@gmail.com>
2023-05-26 12:57:00 +1000

    contact transition no spring shit

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -9,7 +9,7 @@ const itemVariants: Variants = {
   open: {
     opacity: 1,
     y: 0,
-    transition: { type: "spring", stiffness: 300, damping: 24 },
+    transition: { duration: 0.2  },
   },
   closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
 }
