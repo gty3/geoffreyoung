e5e75e5f936eaaac449586e01d088ed9fdec46a0
gty <gefyoung@gmail.com>
2023-05-26 12:21:14 +1000

    background fix? window open instead of next link - nextlink was causing issue opening in prod with icons updating

--- app/imGeoff.tsx
+++ app/imGeoff.tsx
@@ -8,15 +8,15 @@ export default function ImGeoff() {
   const Button = () => {
     return (
       <motion.button whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.96 }}>
-        <Link
-          href="http://instagram.com/_u/geoffreyoung/"
+        <div
+          onClick={() => {window && window.open("http://instagram.com/_u/geoffreyoung/")}}
           className="flex flex-row justify-center w-40 p-1 border rounded-lg shadow-sm bg-clip-padding bg-white/20"
         >
           <Image src="/insta.png" alt="insta" height={30} width={30} />
           <p className="text-sm font-semibold text-[#1d9bf0] mt-1.5 mx-1">
             {"Hi, Im Geoff"}
           </p>
-        </Link>
+        </div>
       </motion.button>
     )
   }
