5c442d400d451bd01eeb16b46baf58aff12a9263
gty <gefyoung@gmail.com>
2023-06-13 15:27:48 +1000

    trying different truncate library

--- app/posts/sidebar.tsx
+++ app/posts/sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { compareDesc, format, parseISO } from "date-fns"
 import { allPosts, Post } from "contentlayer/generated"
 import Header from "../header"
-import TextTruncate from "react-text-truncate"
+import Truncate from 'react-truncate'
 import Image from "next/image"
 
 function PostCard(post: Post) {
@@ -13,15 +13,12 @@ function PostCard(post: Post) {
       <h2 className="flex flex-row">
         <Link
           href={post.slug}
-          className="flex px-3 py-2 mb-1 text-xs rounded-lg w-[48rem] hover:bg-blue-50 focus:bg-blue-100"
+          className="flex px-3 py-2 mb-1 text-xs rounded-lg w-96 hover:bg-blue-50 focus:bg-blue-100"
         >
           {post.image && <Image className="mr-2" src={post.image} width={20} height={20} alt="logo" />}
-          <TextTruncate
-            line={1}
-            element="span"
-            truncateText="…"
-            text={post.title}
-          />
+          <Truncate lines={1} ellipsis={<span>...</span>}>
+                {post.title}
+            </Truncate>
         </Link>
         
       </h2>
