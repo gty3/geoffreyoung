954bea5bd31a7a18d574056d86cdadce667c967d
gty <gefyoung@gmail.com>
2023-06-20 13:29:21 +1000

    trying out custom media query uselayouteffect to get mobile image for bookmarks comp

---
+++ app/components/useMediaQuery.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useState, useLayoutEffect, useEffect } from "react";
+
+
+const getMatches = (query: string): boolean => {
+  // Prevents SSR issues
+  if (typeof window !== 'undefined') {
+    return window.matchMedia(query).matches;
+  }
+  return false;
+};
+
+/**
+ * Returns true if the given media query matches.
+ * Returns undefined until the first render occured to prevent hydration errors.
+ * @param query A media query as string
+ * @returns
+ */
+export function useMediaQuery(query: string): boolean | undefined {
+  const [hasMatch, setHasMatches] = useState<boolean>(getMatches(query));
+  const [initialLoad, setInitialLoad] = useState(true);
+
+  useLayoutEffect(() => {
+    if (initialLoad) {
+      setInitialLoad(false);
+    }
+  }, []);
+
+  function handleChange() {
+    setHasMatches(getMatches(query));
+  }
+
+  useEffect(() => {
+    const matchMedia = window.matchMedia(query);
+    handleChange();
+    matchMedia.addEventListener('change', handleChange);
+    return () => {
+      matchMedia.removeEventListener('change', handleChange);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return initialLoad ? undefined : hasMatch;
+}
--- app/landingPage/development.tsx
+++ app/landingPage/development.tsx
@@ -1,15 +1,21 @@
+"use client"
+
 import Image from "next/image"
 import World from "./development/world"
-import Zoom from 'react-medium-image-zoom'
-import 'react-medium-image-zoom/dist/styles.css'
+import Zoom from "react-medium-image-zoom"
+import "react-medium-image-zoom/dist/styles.css"
 import Bookmarks from "/public/bookmarks1.png"
+import { useMediaQuery } from "../components/useMediaQuery"
 
 export default function Development() {
+  const isMobile = useMediaQuery(`(max-width: 767.98px)`)
   return (
     <div className="backdrop-blur-none">
       <div className="justify-center max-w-6xl px-4 mx-auto mt-20 sm:space-x-20 sm:flex backdrop-blur-none">
         <div className="z-50 max-w-md my-10 mt-20">
-          <div className="pb-2 text-3xl font-bold sm:text-4xl">My web development</div>
+          <div className="pb-2 text-3xl font-bold sm:text-4xl">
+            My web development
+          </div>
           <div className="mt-4 text-lg sm:text-xl">
             {`I started my journey learning about serverless architecture and infrastructure as code. I worked with yaml files and the Serverless Framework as well as AWS CDK.`}
           </div>
@@ -18,17 +24,35 @@ export default function Development() {
       </div>
       <div className="z-50 flex flex-col-reverse justify-center max-w-6xl px-4 mx-auto mb-20 -mt-10 sm:flex-row sm:space-x-20 sm:flex">
         <Zoom>
-          <Image
+          {(!isMobile || isMobile === undefined) && (
+            <Image
+              height={1390}
+              width={1951}
+              src="/bookmarks1.png"
+              alt="Web development bookmarks - high quality"
+            />
+          )}
+          {(isMobile || isMobile === undefined) && (
+            <Image
+            height={347.5}
+            width={487.75}
+              src="/bookmarks1.png"
+              alt="Web development bookmarks - low quality"
+            />
+          )}
+          {/* <Image
             className="rounded-lg sm:max-w-lg"
             src="/bookmarks1.png"
             alt="web development bookmarks"
             // loading="lazy"
             height={1390}
             width={1951}
-          ></Image>
+          ></Image> */}
         </Zoom>
         <div className="order-1 max-w-md my-10 text-lg sm:text-xl">
-          {"I've consumed a plethora of resources, collecting bookmarks along the way. I spend most of my time now with Next.js, TypeScript, and Node.js."}
+          {
+            "I've consumed a plethora of resources, collecting bookmarks along the way. I spend most of my time now with Next.js, TypeScript, and Node.js."
+          }
         </div>
       </div>
     </div>
