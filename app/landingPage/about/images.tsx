ae9880db2f83faa7d086df000f586e61fa520876
gty <gefyoung@gmail.com>
2023-06-20 15:06:29 +1000

    commented out mobile vs desktop img sourcing

--- app/landingPage/about/images.tsx
+++ app/landingPage/about/images.tsx
@@ -4,21 +4,28 @@ import { useEffect, useState } from "react"
 import { useTransition, animated } from "@react-spring/web"
 
 import Image from "next/image"
-// import Mtb1 from "public/about/desktop/mtb1.jpg"
-import Slackline1 from "public/about/mobile/slackline1.jpg"
-import Freedive from "public/about/mobile/freedive.jpg"
-import Mtb2 from "public/about/mobile/mtb2.jpg"
-import Mtb3 from "public/about/mobile/mtb3.jpg"
-import Paddleboard from "public/about/mobile/paddleboard.jpg"
-import Snow from "public/about/mobile/snow.jpg"
-import Mtb1 from "public/about/mobile/mtb1.jpg"
+// import Slackline1 from "public/about/mobile/slackline1.jpg"
+// import Freedive from "public/about/mobile/freedive.jpg"
+// import Mtb2 from "public/about/mobile/mtb2.jpg"
+// import Mtb3 from "public/about/mobile/mtb3.jpg"
+// import Paddleboard from "public/about/mobile/paddleboard.jpg"
+// import Snow from "public/about/mobile/snow.jpg"
+// import Mtb1 from "public/about/mobile/mtb1.jpg"
 
+import DSlackline1 from "public/about/desktop/slackline1.jpg"
+import DFreedive from "public/about/desktop/freedive.jpg"
+import DMtb2 from "public/about/desktop/mtb2.jpg"
+import DMtb3 from "public/about/desktop/mtb3.jpg"
+import DPaddleboard from "public/about/desktop/paddleboard.jpg"
+import DSnow from "public/about/desktop/snow.jpg"
+import DMtb1 from "public/about/desktop/mtb1.jpg"
 import { useMediaQuery } from "@/app/components/useMediaQuery"
 
 
 
 export default function Images() {
-  const slides = [Mtb1, Slackline1, Freedive, Mtb2, Mtb3, Paddleboard, Snow]
+  // const mobileSlides = [Mtb1, Slackline1, Freedive, Mtb2, Mtb3, Paddleboard, Snow]
+  const slides = [DMtb1, DSlackline1, DFreedive, DMtb2, DMtb3, DPaddleboard, DSnow]
 
   const [matches, setMatches] = useState<boolean>()
     // const matches = typeof window !== "undefined" && window.matchMedia("(min-width: 768px)").matches
@@ -39,15 +46,15 @@ export default function Images() {
     "/about/desktop/freedive.jpg",
     "/about/desktop/snow.jpg", 
   ]
-  const mobileImages = [
-    "/about/mobile/mtb1.jpg",
-    "/about/mobile/slackline1.jpg",
-    "/about/mobile/mtb2.jpg",
-    "/about/mobile/mtb3.jpg",
-    "/about/mobile/paddleboard.jpg",
-    "/about/mobile/freedive.jpg",
-    "/about/mobile/snow.jpg", 
-  ]
+  // const mobileImages = [
+  //   "/about/mobile/mtb1.jpg",
+  //   "/about/mobile/slackline1.jpg",
+  //   "/about/mobile/mtb2.jpg",
+  //   "/about/mobile/mtb3.jpg",
+  //   "/about/mobile/paddleboard.jpg",
+  //   "/about/mobile/freedive.jpg",
+  //   "/about/mobile/snow.jpg", 
+  // ]
 
   const [index, set] = useState(0)
   const transitions = useTransition(index, {
@@ -80,7 +87,7 @@ export default function Images() {
           <Image
             className="rounded-lg "
             //  src={`https://ik.imagekit.io/ml8s1f1667/${images[i]}?tr=w-1080,h-800`}
-             src={matches ? slides[i] : mobileImages[i]}
+             src={slides[i]}
             // width={800}
             // height={600}
             alt={images[i]}
